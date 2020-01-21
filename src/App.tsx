9686030b1d124b4f60feb1dcd16ca05ef8d79b09
Chris Flannery <chriswillsflannery@gmail.com>
2020-01-21 15:10:12 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, { date: Date }> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  componentDidMount() {
+    setInterval((t) => {
+      this.setState({
+        date: new Date()
+      })
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+      </>
+    )
+  }
 }
 
 export default App;
